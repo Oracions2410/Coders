4431fc80d3ec0ebc39ae0f98ac7e531b344560c2
Louis Mary MONJENGUE <Oracions2410@gmail.com>
2019-03-23 01:16:18 +0100

    Create a web Design

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+define('ROOT', dirname(__DIR__));
+define('WEBROOT', __DIR__);
+
+
+require WEBROOT."/templates/default/default.php";
+
+
---
+++ public/templates/default/default.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Coders</title>
+</head>
+<body>
+    
+
+  
+    <div id="content">
+        <nav class=".content-header">
+              <p>Coders - Web Developpement<p>
+        </nav>
+       
+    </div>
+    <div id="main-image"></div>
+    <p class="nav-welcome">Welcome Everyone !</p>
+    
+</body>
+</html>
